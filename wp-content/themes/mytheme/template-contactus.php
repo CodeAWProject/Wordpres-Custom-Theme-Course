2f3809dc78e52f7acfc34128bb9b1cb6874a8561
CodeAWProject <activityware@gmail.com>
2024-01-14 18:05:22 +0100

    Implementing a 'Copy Posts' plugin and displaying all posts on the archive page within the blog category, using the section archive

--- wp-content/themes/mytheme/template-contactus.php
+++ wp-content/themes/mytheme/template-contactus.php
@@ -7,22 +7,23 @@ Template Name: Contact Us
 
 <?php get_header();?>
 
+<section class="page-wrap">
+    <div class="container">
+        <h1><?php the_title(); ?></h1>
 
-<div class="container">
-    <h1><?php the_title(); ?></h1>
+        <div class="row">
+            <div class="col-lg-6">
+                This where the contact form goes
+            </div>
 
-    <div class="row">
-        <div class="col-lg-6">
-            This where the contact form goes
+            <div class="col-lg-6">
+                <?php get_template_part('includes/section', 'content');?>
+            </div>
         </div>
 
-        <div class="col-lg-6">
-            <?php get_template_part('includes/section', 'content');?>
-        </div>
+        
     </div>
-
-    
-</div>
+</section>
 
 
 
