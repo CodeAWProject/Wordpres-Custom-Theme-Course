774d92567af9d02d8ad2f94f32b231e067c38cbb
CodeAWProject <activityware@gmail.com>
2024-01-15 00:46:30 +0100

    Creating a single car page along with 'section-cars' and 'archive-cars' templates

--- wp-content/themes/mytheme/includes/section-cars.php
+++ wp-content/themes/mytheme/includes/section-cars.php
@@ -1,16 +1,11 @@
 <?php if( have_posts() ): while( have_posts() ): the_post(); ?>
 
 
-    <p><?php echo get_the_date('l F, Y'); ?></p>
+    
 
     <?php the_content();?>
 
-    <?php 
-    $fname = get_the_author_meta('first_name');
-    $lname = get_the_author_meta('last_name');
-    ?>
-
-    <p>Posted by <?php echo $fname; ?> <?php echo $lname; ?></p>
+    
 
     <?php
 
@@ -33,7 +28,7 @@
             </a>
         <?php endforeach;?>
 
-
+        <p><?php echo get_the_date('l F, Y'); ?></p>
             
         <?php //comments_template(); ?>
 
