4092f117139179e6952ab8726961b385e858e5f6
CodeAWProject <activityware@gmail.com>
2024-01-15 12:41:30 +0100

    Displaying field groups from ACF on the single car page

--- wp-content/themes/mytheme/single-cars.php
+++ wp-content/themes/mytheme/single-cars.php
@@ -33,6 +33,43 @@
                 
             </ul>
 
+            <h3>Features</h3>
+
+            <ul>
+                <?php if(have_rows('features')):?>
+                    <?php while(have_rows('features')): the_row();
+                    $feature1 = get_sub_field('feature1');
+                    $feature2 = get_sub_field('feature2');
+                    $feature3 = get_sub_field('feature3');
+
+                    ?>   
+
+                    <li><?php echo $feature1; ?></li>
+                    <li><?php echo $feature2; ?></li>
+                    <li><?php echo $feature3; ?></li>
+
+                    <?php endwhile; ?>
+
+                <?php endif; ?>
+            </ul>
+
+            <ul>
+            <?php if(have_rows('gallery')):?>
+                    <?php while(have_rows('gallery')): the_row();
+                    $image1 = get_sub_field('image1');
+                    $image2 = get_sub_field('image2');   
+                    $image3 = get_sub_field('image3');       
+                    ?>   
+
+                    <img src="<?php echo $image1; ?>" class="img-fluid">
+                    <img src="<?php echo $image2; ?>" class="img-fluid">
+                    <img src="<?php echo $image3; ?>" class="img-fluid">
+
+                    <?php endwhile; ?>
+
+                <?php endif; ?>
+            </ul>
+
         </div>
     </div>
 
