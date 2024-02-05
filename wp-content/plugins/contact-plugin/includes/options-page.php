6a5c096e284ddb5b1587efdede3138783be3ddf0
CodeAWProject <activityware@gmail.com>
2024-02-05 19:39:49 +0100

    Creating theme options and enabling them for the WordPress admin

--- wp-content/plugins/contact-plugin/includes/options-page.php
+++ wp-content/plugins/contact-plugin/includes/options-page.php
@@ -1,13 +1,21 @@
 <?php 
 
-use Carbon_Fields\Container;
 use Carbon_Fields\Field;
-
+use Carbon_Fields\Container;
 
 
 add_action('after_setup_theme', 'load_carbon_fields');
+add_action('carbon_fields_register_fields', 'create_option_page');
 
 function load_carbon_fields()
 {
     \Carbon_Fields\Carbon_Fields::boot();
+}
+
+function create_option_page()
+{
+    Container::make( 'theme_options', __( 'Theme Options' ) )
+    ->add_fields( array(
+        Field::make( 'text', 'contact_plugin_recipients', __( 'Recipient Email' ) ),
+    ) );
 }
