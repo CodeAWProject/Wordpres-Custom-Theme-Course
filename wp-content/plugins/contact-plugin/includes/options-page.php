7e822b3cafe9725ee1d18d345a266560b966e5db
CodeAWProject <activityware@gmail.com>
2024-02-06 21:03:18 +0100

    Setting placeholders and providing helpful messages for the fields

--- wp-content/plugins/contact-plugin/includes/options-page.php
+++ wp-content/plugins/contact-plugin/includes/options-page.php
@@ -16,6 +16,11 @@ function create_option_page()
 {
     Container::make( 'theme_options', __( 'Theme Options' ) )
     ->add_fields( array(
-        Field::make( 'text', 'contact_plugin_recipients', __( 'Recipient Email' ) ),
+        Field::make( 'text', 'contact_plugin_recipients', __( 'Recipient Email' ) )
+        ->set_attribute( 'placeholder', 'eg. your@email.com' )
+        ->set_help_text( 'The email that the form is submitted to' ),
+        Field::make( 'textarea', 'contact_plugin_message', __( 'Confirmation Message' ) )
+        ->set_attribute( 'placeholder', 'Enter confirmation message' )
+        ->set_help_text( 'Type the message you want the submitter to receive')
     ) );
 }
